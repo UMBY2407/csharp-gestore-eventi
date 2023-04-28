445633fcaecbada669cf8776d2cac5d411432b9c
UMBY2407 <umby2407@gmail.com>
2023-04-28 12:21:14 +0200

    Created ProgrammaEventi class, attributes, constructor, AddEvento() and EventiPerData(). Milestone3

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+	public class ProgrammaEventi
+	{
+		//ATTRIBUTI
+		string titolo;
+		List<Evento> eventi;
+
+		//COSTRUTTORE
+		public ProgrammaEventi(string titoloEvento)
+		{
+			this.titolo = titoloEvento;
+			this.eventi = new List<Evento>();
+		}
+
+		//METODI
+		public void AddEvento(Evento eventoDaAggiungere)
+		{
+			eventi.Add(eventoDaAggiungere);
+			Console.WriteLine($"{eventoDaAggiungere} aggiunto alla lista Eventi!");
+		}
+
+		public void EventiPerData(DateTime data)
+		{
+			List<Evento> eventiPerData = new List<Evento>();
+			foreach (Evento eventoLetto in eventi)
+			{
+				if(eventoLetto.GetData() == data)
+				{
+					eventiPerData.Add(eventoLetto);
+				}
+			}
+			
+			foreach(Evento eventoLetto in eventiPerData)
+			{
+				Console.WriteLine(data + " - " + eventoLetto.GetTitolo());
+			}
+		}
+	}
+}
