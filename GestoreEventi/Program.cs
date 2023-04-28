1fb63611b0b8a76e065c9d675030591756cf0bf1
UMBY2407 <umby2407@gmail.com>
2023-04-28 18:00:37 +0200

    Added exception controls on events in program.cs

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -39,41 +39,94 @@ try
 	Console.WriteLine("Numero di posti disponibili: " + mioEvento1.GetCapienzaMassimaEvento());
 	Console.WriteLine("");
 
-	//CREAZIONE PROGRAMMA EVENTI
-	Console.Write("Inserisci il nome del tuo programma di eventi: ");
-	string titoloProgramma = Console.ReadLine();
-	if(titoloProgramma == "")
-	{
-		throw new ArgumentException("il titolo inserito risulta nullo", "titoloProgramma");
-	}
 
+
+//CREAZIONE PROGRAMMA EVENTI
+Console.Write("Inserisci il nome del tuo programma di eventi: ");
+	string titoloProgramma = Console.ReadLine();
 	ProgrammaEventi mioProgramma = new ProgrammaEventi(titoloProgramma);
-	
+
 	Console.Write("Indica il numero di eventi da inserire: ");
 	int numeroEventi = int.Parse(Console.ReadLine());
 
-	//CREAZIONE ED INSERIMENTO EVENTI NEL PROGRAMMA CREATO
+//CREAZIONE ED INSERIMENTO EVENTI NEL PROGRAMMA CREATO
 	for (int i = 0; i < numeroEventi; i++)
 	{
-		Console.WriteLine("");  // PER FORMATTAZIONE
-		Console.Write($"Inserire il nome del {i + 1}° evento: ");
-		nomeEvento = Console.ReadLine();
+		try
+		{
+			Console.WriteLine("");  // PER FORMATTAZIONE
+			Console.Write($"Inserire il nome del {i + 1}° evento: ");
+			nomeEvento = Console.ReadLine();
+			if(nomeEvento == "")
+			{
+				throw new ArgumentException($"Il titolo del {i+1}° evento risulta nullo!", "nomeEvento");
+			}
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"Ci sono stati degli errori durante l'inserimento del titolo dell'evento n.{i+1}!");
+			Console.Write(e.Message);
+			while (nomeEvento == "")
+			{
+				Console.Write("\nReinserisci il titolo dell'evento! ");
+				nomeEvento = Console.ReadLine() ;
+			}
+		}
+		try
+		{
+			Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
+			dataEventoStringa = Console.ReadLine();
+			if (dataEventoStringa == "")
+			{
+				throw new ArgumentException($"La data del {i + 1}° evento risulta nullo!", "dataEventoStringa");
+			}
+			dataEvento = DateTime.Parse(dataEventoStringa);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"Ci sono stati degli errori durante l'inserimento della data dell'evento n.{i + 1}!");
+			Console.Write(e.Message);
+			while (dataEventoStringa == "")
+			{
+				Console.Write("\nReinserisci la data dell'evento! ");
+				dataEventoStringa = Console.ReadLine();
+			}
+		}
+		try
+		{
+			Console.Write("Inserisci il numero di posti totali: ");
+			capienzaMaxEvento = int.Parse(Console.ReadLine());
+			if(capienzaMaxEvento == 0 || capienzaMaxEvento < 0)
+			{
+				throw new ArgumentException($"La capienza massima del {i + 1}° evento risulta vuota o negativa!", "capienzaMaxEvento");
+			}
+			
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine($"Ci sono stati degli errori durante l'inserimento dei posti totali dell'evento n.{i+1}!");
+			Console.Write(e.Message);
+			while (capienzaMaxEvento == 0 || capienzaMaxEvento < 0)
+			{
+				Console.Write("\nReinserisci il numero di posti totale dell'evento! ");
+				capienzaMaxEvento = int.Parse(Console.ReadLine());
+			}
+		}
+		try
+		{
+			Evento eventoProgramma = new Evento(nomeEvento, dataEvento, capienzaMaxEvento);
+			mioProgramma.AddEvento(eventoProgramma);
 
-		Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
-		dataEventoStringa = Console.ReadLine();
-		dataEvento = DateTime.Parse(dataEventoStringa);
-
-		Console.Write("Inserisci il numero di posti totali: ");
-		capienzaMaxEvento = int.Parse(Console.ReadLine());
-
-		Evento eventoProgramma = new Evento(nomeEvento, dataEvento, capienzaMaxEvento);
-		mioProgramma.AddEvento(eventoProgramma);
-		
-		Console.WriteLine("");  // PER FORMATTAZIONE
+			Console.WriteLine("");  // PER FORMATTAZIONE
+		}catch(Exception e)
+		{
+			Console.WriteLine("Ci sono stati degli errori durante la crezione dell'oggetto eventoProgramma!");
+			Console.Write(e.Message);
+		}
 	}
-	//USO DEI METODI CLASSE PROGRAMMA EVENTO
-	
-	Console.WriteLine("");
+//USO DEI METODI CLASSE PROGRAMMA EVENTO
+
+Console.WriteLine("");
 	mioProgramma.ContaNumeroEventi();
 	Console.WriteLine("Ecco il tuo programma eventi:");
 	Console.WriteLine(mioProgramma.TitoloEDataEventiInListaToString());
@@ -85,7 +138,7 @@ try
 
 	mioProgramma.EventiPerData(eventoPerData);
 }
-catch(Exception e)
+catch (Exception e)
 {
 	Console.WriteLine("Qualcosa è andato storto...");
 	Console.WriteLine(e.Message);
