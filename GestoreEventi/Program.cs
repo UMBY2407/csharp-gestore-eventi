6360382cc3c08a17b43cd3578b93be08c7df5904
UMBY2407 <umby2407@gmail.com>
2023-04-28 11:30:27 +0200

    Added User input, created Evento object and used PrenotaPosti method in Milestone2

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,30 @@
 ﻿using GestoreEventi;
-Console.WriteLine("Hello, World!");
+
+try
+{
+//INPUT UTENTE	
+	Console.Write("Inserisci il nome dell'Evento: ");
+	string nomeEvento = Console.ReadLine();
+
+	Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
+	string dataEventoStringa = Console.ReadLine();
+	DateTime dataEvento = DateTime.Parse(dataEventoStringa);
+
+	Console.Write("Inserire il numero di posti totali: ");
+	int capienzaMaxEvento = int.Parse(Console.ReadLine());
+
+	Console.Write("Quanti posti vuoi prenotare? ");
+	int postiPrenotati = int.Parse(Console.ReadLine());
+
+//CREAZIONE OGGETTI EVENTO
+	Evento mioEvento1 = new Evento(nomeEvento, dataEvento, capienzaMaxEvento);
+
+//UTILIZZO METODI DELLA CLASSE EVENTO
+	mioEvento1.PrenotaPosti(postiPrenotati);
+
+}
+catch(Exception e)
+{
+	Console.WriteLine("Qualcosa è andato stoto...");
+	Console.WriteLine(e.Message);
+}
