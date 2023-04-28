02b1b0b460f02d81eef699ce5907c06a3f2055c9
UMBY2407 <umby2407@gmail.com>
2023-04-28 10:11:02 +0200

    Created Attributes, Getters, Setters ando Constructor in Milestone1

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+	public class Evento
+	{
+		//ATTRIBUTI
+		private string titolo;
+		private DateTime dataEvento = new DateTime();
+		private int CapienzaMassimaEvento;
+		private int numeroPostiPrenotati = 0;
+
+		//GETTERS
+		public string GetTitolo()
+		{
+			return this.titolo;
+		}
+
+		public DateTime GetData()
+		{
+			return this.dataEvento;
+		}
+
+		public int GetCapienzaMassimaEvento()
+		{
+			return this.CapienzaMassimaEvento;
+		}
+
+		public int GetNumeroPostiPrenotati()
+		{
+			return this.numeroPostiPrenotati;
+		}
+
+		//SETTERS
+		public string SetTitolo(string nuovoTitolo)
+		{
+			if (nuovoTitolo == "")
+			{
+				throw new ArgumentException("Il titolo nuovo risulta vuoto!","nuovoTitolo");
+			}
+			this.titolo = nuovoTitolo;
+			return this.titolo;
+		}
+
+		public DateTime SetData(DateTime nuovaData)
+		{
+			int result = DateTime.Compare(this.dataEvento, nuovaData);
+			if(result < 0)
+			{
+				throw new ArgumentException("La data inserita risulta già passata!", "nuovaData");
+			}
+			this.dataEvento = nuovaData;
+			return this.dataEvento;
+		}
+
+		//COSTRUTTORE
+		public Evento(string titoloEvento, DateTime data, int postiDisponibili)
+		{
+			this.titolo = titoloEvento;
+			this.dataEvento = data;
+			this.CapienzaMassimaEvento = postiDisponibili;
+		}
+	}
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using GestoreEventi;
 Console.WriteLine("Hello, World!");
